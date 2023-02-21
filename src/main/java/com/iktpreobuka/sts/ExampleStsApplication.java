f8b42c8f499a1929e148b8dc155fc39db2b9f480
cikaricIvan <cikaricivan@yahoo.com>
2023-02-21 19:46:30 +0100

    my first message

---
+++ src/main/java/com/iktpreobuka/sts/ExampleStsApplication.java
@@ -0,0 +1,13 @@
+package com.iktpreobuka.sts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleStsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleStsApplication.class, args);
+	}
+
+}
